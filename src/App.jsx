394ea50ccc5919b7e54405eb24048e8d032a56f6
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-12-30 09:50:40 -0800

    Loading page done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Auth from './components/Auth/Auth'
 import { useDispatch, useSelector } from 'react-redux'
 import Home from './components/Home/Home'
 import { auth } from './redux/actions/authAction'
+import { IoLogoIonitron } from "react-icons/io";
 import {
   setPublicRooms,
   setPrivateRooms,
@@ -14,12 +15,16 @@ import DesktopView from './components/DesktopView/DesktopView'
 function App() {
 
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true)
   const user = useSelector((state) => state.authData.session);
   const menu = useSelector((state) => state.menuData.selectedMenu);
 
   const getSession = async (e) => {
     const { data, error } = await supabase.auth.getSession();
     dispatch(auth(data?.session));
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
   };
 
   const getPublicRooms = async (e) => {
@@ -55,16 +60,28 @@ function App() {
 
   return (
     <div>
-      {
-        user ? (
-          <>
-            <MobileView/>
-            <DesktopView/>
-          </>
-        ) : <Auth/>
-      }
+      {loading ? (
+        <div className="w-full h-screen bg-[#211F2C] cursor-pointer group flex flex-row gap-2 justify-center items-center p-5">
+          <span className="flex items-center justify-center gap-3 animate-ping">
+            <IoLogoIonitron
+              fontSize={50}
+              className="group-hover:text-teal-300 text-teal-400"
+            />
+            <h1 className="group-hover:text-teal-300 text-3xl font-popins font-bold text-teal-500">
+              ChatBuddy
+            </h1>
+          </span>
+        </div>
+      ) : user ? (
+        <>
+          <MobileView />
+          <DesktopView />
+        </>
+      ) : (
+        <Auth />
+      )}
     </div>
-  )
+  );
 }
 
 export default App
