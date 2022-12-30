a3a84f74f9b1faf46a2bfe5bb5f5d01505494e0f
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-12-30 09:34:41 -0800

    Auth page done

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -3,8 +3,10 @@ import { supabase } from '../../utils/supabase/supabaseClient';
 import { auth } from '../../redux/actions/authAction';
 import {image1, image2, image3, image4, image5} from '../../utils/imageHelper'
 import { useDispatch } from 'react-redux';
+import { IoLogoIonitron } from "react-icons/io";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper";
+import bgImage from '../../assets/11.png'
 
 import "swiper/css";
 import "swiper/css/autoplay";
@@ -21,16 +23,18 @@ const Auth = () => {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const changeFormState = (type) => {
     setFormState(type);
     setUserName("");
     setEmail("");
     setPassword("");
+    setConfirmPassword("");
   }
 
   const signIn = async (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     const { data, error } = await supabase.auth.signInWithPassword({
       email: email,
       password: password,
@@ -44,7 +48,7 @@ const Auth = () => {
   }
 
   const signUp = async (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     const { data, error } = await supabase.auth.signUp({
       email: email,
       password: password,
@@ -62,6 +66,11 @@ const Auth = () => {
     }
   }
 
+  const formSubmit = () => {
+    formState === 'login' && signIn();
+    formState === 'register' && signUp();
+  }
+
   const slideChange = () => {
     const images = document.querySelectorAll('.s-image');
     slider.slides.forEach((slide, index) => {
@@ -72,165 +81,152 @@ const Auth = () => {
   }
 
   return (
-    <div className="formDiv flex h-screen w-full items-center justify-between bg-[#211F2C] bg-cover bg-no-repeat">
-      <div className="w-full h-full flex items-center justify-center border">
-        <Swiper
-          modules={[Autoplay, EffectFade]}
-          effect={"fade"}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false
-          }}
-          className="w-72"
-          // spaceBetween={50}
-          slidesPerView={1}
-          onSlideChange={slideChange}
-          onSwiper={(swiper) => setSlider(swiper)}
-        >
-          {images.map((image, index) => {
-            return (
-              <SwiperSlide className="flex items-center justify-center h-80 w-80">
-                <img src={image} alt="" srcset="" className="s-image w-full h-full" id={`img_${index}`}/>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        {/* div
-        <img src={image5} alt="" srcset="" className=""/> */}
-      </div>
-
-      <div className="w-full flex items-center justify-center">
-        <div className="h-72 w-52 bg-red-300"></div>
-      </div>
-
-      {/* {formState === "login" && (
-        <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-          <div className="text-white">
-            <div className="mb-8 flex flex-col items-center">
-              <img src={Logo} width="250" alt="" srcSet="" />
+    <section className="h-screen w-full bg-[#211F2C] md:p-10">
+      <div className="px-6 h-full text-gray-800">
+        <div className="flex  flex-col md:flex-row xl:justify-center lg:justify-between justify-center items-center h-full gap-10">
+          <div className="hidden md:block max-w-lg grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-6/12 md:mb-0">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              className="w-full"
+              alt="Sample image"
+            />
+          </div>
+          <div className="rounded-3xl flex flex-col gap-5 bg-[#181725] p-10 xl:ml-20 xl:w-6/12 lg:w-6/12 md:w-6/12 max-w-md shadow-lg shadow-teal-800 md:mb-0 md:m-10 md:shrink-0 basis-auto">
+            <div className="cursor-pointer group flex flex-row gap-2 justify-center items-center p-5">
+              <IoLogoIonitron
+                fontSize={50}
+                className="group-hover:text-teal-300 text-teal-400"
+              />
+              <h1 className="group-hover:text-teal-300 text-3xl font-popins font-bold text-teal-500">
+                ChatBuddy
+              </h1>
             </div>
-            <form action="#">
-              <div className="mb-4 text-lg">
+            <form className="flex flex-col gap-4 mt-3">
+              {formState === "register" && (
+                <div className="">
+                  <input
+                    type="text"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    className="bg-[#181725] hover:placeholder:text-teal-400 hover:border-teal-400 font-popins placeholder:font-popins form-control block w-full px-4 py-2 font-normal text-teal-700 bg-clip-padding border border-solid border-teal-700 placeholder:text-teal-700 placeholder:text-sm text-md rounded transition ease-in-out m-0 focus:text-teal-400 focus:bg-transparent focus:border-teal-300 focus:outline-none"
+                    id="userName"
+                    placeholder="User name"
+                  />
+                </div>
+              )}
+
+              <div className="">
                 <input
-                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-300 shadow-lg outline-none backdrop-blur-md"
-                  type="text"
-                  name="email"
-                  placeholder="Enter email"
+                  type="email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
+                  className="hover:placeholder:text-teal-400 hover:border-teal-400 bg-[#181725] font-popins placeholder:font-popins form-control block w-full px-4 py-2 font-normal text-teal-700 bg-clip-padding border border-solid border-teal-700 placeholder:text-teal-700 placeholder:text-sm text-md rounded transition ease-in-out m-0 focus:text-teal-400 focus:bg-transparent focus:border-teal-300 focus:outline-none"
+                  id="email"
+                  placeholder="Email address"
                 />
               </div>
 
-              <div className="mb-4 text-lg flex flex-col">
+              {/* <!-- Password input --> */}
+              <div className="">
                 <input
-                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-300 shadow-lg outline-none backdrop-blur-md"
-                  type="Password"
-                  name="password"
-                  placeholder="Enter password"
+                  type="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
+                  className="hover:border-teal-400 hover:placeholder:text-teal-400 bg-[#181725] font-popins placeholder:font-popins form-control block w-full px-4 py-2 font-normal text-teal-700 bg-clip-padding border border-solid border-teal-700 placeholder:text-teal-700 placeholder:text-sm text-md rounded transition ease-in-out m-0 focus:text-teal-400 focus:bg-transparent focus:border-teal-300 focus:outline-none"
+                  id="password"
+                  placeholder="Password"
                 />
-                <a
-                  type="button"
-                  href="#"
-                  className="underline  text-center text-[18px] mt-3 text-gray-300"
-                  // onClick={() => setFormState("forgetPassword")}
-                >
-                  Forget the password?
-                </a>
               </div>
-              <div className="mt-8 flex justify-center text-lg text-black">
+
+              {formState === "forgetPassword" && (
+                <div className="">
+                  <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    className="hover:border-teal-400 hover:placeholder:text-teal-400 bg-[#181725] font-popins placeholder:font-popins form-control block w-full px-4 py-2 font-normal text-teal-700 bg-clip-padding border border-solid border-teal-700 placeholder:text-teal-700 placeholder:text-sm text-md rounded transition ease-in-out m-0 focus:text-teal-400 focus:bg-transparent focus:border-teal-300 focus:outline-none"
+                    id="passwordConfirm"
+                    placeholder="Confirm Password"
+                  />
+                </div>
+              )}
+
+              <div className="text-center lg:text-left mt-3 mb-2">
                 <button
                   type="submit"
-                  className="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600"
-                  onClick={(e) => signIn(e)}
+                  className="inline-block px-7 py-2 font-popins bg-teal-700 text-gray-900 text-sm leading-snug uppercase rounded shadow-md shadow-gray-900 hover:bg-teal-400 hover:shadow-lg focus:bg-teal-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-teal-400 active:shadow-lg transition duration-150 ease-in-out"
+                  onClick={formSubmit}
                 >
-                  Login
+                  {formState === "login" && "Login"}
+                  {formState === "register" && "Register"}
+                  {formState === "forgetPassword" && "Change Password"}
                 </button>
               </div>
-              <div className="mt-8 flex justify-center text-lg text-black">
-                <p className="text-gray-300">
-                  New to here?
-                  <a
-                    type="button"
-                    href="#"
-                    className="ml-2 underline "
-                    onClick={() => changeFormState("signUp")}
+
+              <div>
+                {formState === "login" && (
+                  <p className="cursor-pointer text-sm font-popins mb-0 text-teal-700 hover:text-teal-400 group">
+                    Don't have an account?
+                    <button
+                      onClick={() => changeFormState("register")}
+                      className="ml-2 underline text-teal-600 group-hover:text-teal-300"
+                    >
+                      Register
+                    </button>
+                  </p>
+                )}
+                {formState === "forgetPassword" && (
+                  <p className="cursor-pointer text-sm font-popins mb-0 text-teal-700 hover:text-teal-400 group">
+                    Have an account?
+                    <button
+                      onClick={() => changeFormState("login")}
+                      className="ml-2 underline text-teal-600 group-hover:text-teal-300"
+                    >
+                      Login
+                    </button>
+                  </p>
+                )}
+                {formState === "register" && (
+                  <p className="cursor-pointer text-sm font-popins mb-0 text-teal-700 hover:text-teal-400 group">
+                    Have an account?
+                    <button
+                      onClick={() => changeFormState("login")}
+                      className="ml-2 underline text-teal-600 group-hover:text-teal-300"
+                    >
+                      Login
+                    </button>
+                  </p>
+                )}
+              </div>
+
+              <div className="flex justify-between items-center ">
+                {/* <div className="form-group form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                    id="exampleCheck2"
+                  />
+                  <label
+                    className="form-check-label inline-block text-gray-800"
+                    for="exampleCheck2"
                   >
-                    Sign Up
-                  </a>
-                </p>
+                    Remember me
+                  </label>
+                </div> */}
+                {formState !== "forgetPassword" && (
+                  <button
+                    onClick={() => changeFormState("forgetPassword")}
+                    className="font-popins -mt-2 text-sm text-teal-700 hover:text-teal-400 cursor-pointer"
+                  >
+                    Forgot password?
+                  </button>
+                )}
               </div>
             </form>
           </div>
         </div>
-      )}
-      {formState === "signUp" && (
-        <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-          <div className="text-white">
-            <div className="mb-8 flex flex-col items-center">
-              <img src={Logo} width="250" alt="" srcSet="" />
-            </div>
-            <form action="#">
-              <div className="mb-4 text-lg">
-                <input
-                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-300 shadow-lg outline-none backdrop-blur-md"
-                  type="text"
-                  name="userName"
-                  placeholder="Enter user name"
-                  value={userName}
-                  onChange={(e) => setUserName(e.target.value)}
-                />
-              </div>
-
-              <div className="mb-4 text-lg">
-                <input
-                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-300 shadow-lg outline-none backdrop-blur-md"
-                  type="text"
-                  name="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-
-              <div className="mb-4 text-lg flex flex-col">
-                <input
-                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-300 shadow-lg outline-none backdrop-blur-md"
-                  type="Password"
-                  name="password"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="mt-8 flex justify-center text-lg text-black">
-                <button
-                  type="submit"
-                  className="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600"
-                  onClick={(e) => signUp(e)}
-                >
-                  Sign Up
-                </button>
-              </div>
-              <div className="mt-8 flex justify-center text-lg text-black">
-                <p className="text-gray-300">
-                  Allready have an account?
-                  <a
-                    type="button"
-                    href="#"
-                    className="ml-2 underline"
-                    onClick={() => changeFormState("login")}
-                  >
-                    Login
-                  </a>
-                </p>
-              </div>
-            </form>
-          </div>
-        </div>
-      )} */}
-    </div>
+      </div>
+    </section>
   );
 }
 
