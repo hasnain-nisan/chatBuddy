49bbe33c533e496055e6de54a80ef730542abc0c
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2023-01-04 13:43:24 +0600

    Emojis added in desktop view

--- src/components/DesktopView/Input.jsx
+++ src/components/DesktopView/Input.jsx
@@ -7,10 +7,13 @@ import { RiSendPlaneFill } from 'react-icons/ri';
 import { useSelector } from 'react-redux';
 import { supabase } from '../../utils/supabase/supabaseClient';
 import { toast } from 'react-toastify';
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
 
 const Input = () => {
 
   const inputField = useRef();
+  const [showEmojis, setShowEmojis] = useState(false)
   const [message, setMessage] = useState("");
   const user = useSelector((state) => state.authData.session);
   const selected_room = useSelector(
@@ -29,19 +32,28 @@ const Input = () => {
         created_at: moment().format(),
       };
       const { error } = await supabase.from("messages").insert(msg);
+      setShowEmojis(false)
       setMessage("");
     }
   };
 
   const keyDownHandler = event => {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-        if (document.activeElement === inputField.current) {
-          let msg = inputField.current.value
-          sendMessage(msg)
-        }
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (document.activeElement === inputField.current) {
+        let msg = inputField.current.value
+        sendMessage(msg)
       }
-    };
+    }
+  };
+
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setMessage(message + emoji);
+  };
 
   useEffect(() => {
     document.addEventListener('keydown', keyDownHandler);
@@ -52,6 +64,14 @@ const Input = () => {
       id="bottom-navigation"
       className="block relative inset-x-0 bottom-0 z-10 bg-[#211F2C] px-5 py-3"
     >
+      <div className='absolute bottom-16 right-6'>
+        {showEmojis && (
+          <Picker data={data} 
+            onEmojiSelect={addEmoji}                     
+            theme="dark"
+          />
+        )}
+      </div>
       <div class="relative flex justify-between items-center w-full h-10 px-4 rounded-lg bg-[#1E1C26] overflow-hidden">
         <input
           class="peer h-full w-3/5 outline-none text-sm text-teal-700 pl-3 bg-[#1E1C26] placeholder:text-teal-700 placeholder:text-sm"
@@ -64,7 +84,10 @@ const Input = () => {
           ref={inputField}
         />
         <div class="flex justify-between items-center h-full w-20 text-teal-700 pr-2">
-          <MdEmojiEmotions fontSize={18} />
+          <MdEmojiEmotions fontSize={18}
+            className="cursor-pointer"
+            onClick={() => setShowEmojis(!showEmojis)}
+          />
           <HiOutlinePaperClip fontSize={18} />
           <RiSendPlaneFill
             fontSize={18}
